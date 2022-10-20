5be0751180d1264b78647b50d4599308ba035cba
Ayako-q <ischenkalex@gmail.com>
2022-10-21 00:14:01 +0300

    small change

--- src/main/java/com/example/project11/Main.java
+++ src/main/java/com/example/project11/Main.java
@@ -7,11 +7,4 @@ public class Main{
         Collection collection = new Collection();
         collection.start();
     }
-
-    public static String getUserInput(){
-        Scanner in = new Scanner(System.in);
-        return in.nextLine();
-    }
-
-
 }
